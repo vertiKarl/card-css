232f6b7b99a4ab1e94f7cd6201aa4f50bf1ebf95
vertiKarl <vertikarl@proton.me>
2025-08-25 14:35:30 +0200

    new patterns - chromatic - negative - sepia

--- src/lib/CardPattern.ts
+++ src/lib/CardPattern.ts
@@ -1,6 +1,10 @@
 export const CardPatterns = {
   none: "none",
   striped: "striped",
+  rainbow: "rainbow",
+  chromatic: "chromatic",
+  negative: "negative",
+  sepia: "sepia",
 } as const;
 
 export type CardPattern = keyof typeof CardPatterns;
